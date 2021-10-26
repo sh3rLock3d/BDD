d106e18666926b0751006db32eb8ef100c452931
esmaeil1377 <pahang1378@gmail.com>
2021-10-26 20:25:13 +0330

    update MyStepdefs and calculator.feature

--- example/src/test/java/calculator/MyStepdefs.java
+++ example/src/test/java/calculator/MyStepdefs.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -12,6 +13,10 @@ public class MyStepdefs {
     private int value1;
     private int value2;
     private int result;
+
+    private int value1_s2;
+    private String value2_s2;
+    private double result_s2;
 
     @Before
     public void before(){
@@ -34,4 +39,22 @@ public class MyStepdefs {
     public void iExpectTheResult(int arg0) {
         Assert.assertEquals(arg0, result);
     }
+
+    @Given("^One input value (-?\\d+), and opt \"([^\"]*)\"$")
+    public void oneInputValueAndRvs(int arg0, String arg1) throws Throwable {
+        value1_s2 = arg0;
+        value2_s2 = arg1;
+    }
+
+    @When("^I press button$")
+    public void iPressButton() {
+        result_s2 = calculator.press(value1_s2, value2_s2);
+    }
+
+    @Then("^I expect the output ([\\d\\.]+)$")
+    public void iExpectTheOutput(double arg0) {
+        Assert.assertEquals(arg0, result_s2, 0.1);
+    }
+
+
 }
